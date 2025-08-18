553f094526aacb163722f0b0f43b3c4d02eb3b38
Shreyas2545 <shreyashadawale25@gmail.com>
2025-08-18 14:47:12 +0530

    added /aim route in server

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,32 @@ import express from "express"
 
 const app=express();
 
-app.get('/app/info',(req,res) => {
-    res.send('I am Shreyas Hadawale');
-})
+const port = process.env.PORT || 4518;
+// app.get('/app/info',(req,res) => {
+//     res.send('I am Shreyas Hadawale');
+// })
 
-app.get('/app',(req,res) => {
-    res.send('<<<Learning Connecting Backend To Frontend>>>');
-})
+// app.get('/app',(req,res) => {
+//     res.send('<<<Learning Connecting Backend To Frontend>>>');
+// })
+
+// app.get('/app/marks',(req,res) => {
+//     res.send('I want 95%');
+// })
+
 app.get('/app/age',(req,res) => {
     res.send('I am 18 years old');
 })
 
-app.get('/app/marks',(req,res) => {
-    res.send('I want 95%');
+app.get('/app',(req,res) =>{
+    res.send(`Learning Connecting Frontend + Backend `)
 })
 
-const port = process.env.PORT || 4518;
+app.get('/app/aim',(req,res) =>{
+    res.send(`Aim is to master backend`)
+})
 
 app.listen(port,(req,res) => {
-    console.log(`server is runnign at http://localhost:${port}`)
+     console.log(`server is runnign at http://localhost:${port}`)
 });
+
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,60 @@
-import { useState , useEffect } from 'react'
-import axios from 'axios'
+  import { useState , useEffect } from 'react'
+  import axios from 'axios'
 
-function App() {
-  const [name, setName] = useState("");
-  const [age,setAge] = useState();
-  const [marks,setMarks] = useState();
+  function App() {
+    const [name, setName] = useState("");
+    const [age,setAge] = useState();
+    const [marks,setMarks] = useState();
+    const [aim,setAim] = useState();
+    useEffect(() => {
+        axios.get('/app/info')
+        .then((response) => {
+          setName(response.data)
+        })
+        .catch((error)=>{ 
+          console.log(error); 
+        })
+      },[])
+
+      useEffect(()=>{
+        axios.get('/app/aim')
+        .then((response) =>{
+          setAim(response.data)
+        })
+        .catch((error)=>{
+          console.log(error);
+        })
+      })
+
   useEffect(() => {
-      axios.get('/app/info')
-      .then((response) => {
-        setName(response.data)
-      })
-      .catch((error)=>{ 
-        console.log(error); 
-      })
-    },[])
+        axios.get('/app/marks')
+        .then((response) => {
+          setMarks(response.data)
+        })
+        .catch((error)=>{ 
+          console.log(error); 
+        })
+      },[])
 
- useEffect(() => {
-      axios.get('/app/marks')
-      .then((response) => {
-        setMarks(response.data)
-      })
-      .catch((error)=>{ 
-        console.log(error); 
-      })
-    },[])
+  useEffect(() => {
+        axios.get('/app/age')
+        .then((response) => {
+          setAge(response.data)
+        })
+        .catch((error)=>{ 
+          console.log(error); 
+        })
+      },[])
 
- useEffect(() => {
-      axios.get('/app/age')
-      .then((response) => {
-        setAge(response.data)
-      })
-      .catch((error)=>{ 
-        console.log(error); 
-      })
-    },[])
+    return (
+      <>
+      <h1>This is a backend trial </h1>
+      {/* <h1>data Fetched from backend : {name} </h1>
+      <h1>data Fetched from backend : {age} </h1>
+      <h1>data Fetched from backend : {marks} </h1> */}
+      <h3>data Fetched from backend : {aim} </h3>
+      </>
+    )
+  }
 
-  return (
-    <>
-    <h1>This is a backend trial </h1>
-    <h1>data Fetched from backend : {name} </h1>
-    <h1>data Fetched from backend : {age} </h1>
-    <h1>data Fetched from backend : {marks} </h1>
-    </>
-  )
-}
-
-export default App
+  export default App
